6e10ab2c58b0b3c26a6e5bf1afc970a0153b323d
Maxime GINDRE <mout@mout.local>
2018-10-29 16:49:06 +0100

    dev(shopType): add mapping & factory

---
+++ src/Domain/Repository/ShopTypeRepository.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bidule
+ * Date: 29/10/2018
+ * Time: 16:32
+ */
+
+namespace App\Domain\Repository;
+
+
+use App\Domain\Models\ShopType;
+use App\Domain\Repository\Interfaces\ShopTypeRepositoryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+class ShopTypeRepository extends ServiceEntityRepository implements ShopTypeRepositoryInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, ShopType::class);
+    }
+
+    public function getAll(): array
+    {
+        return $this->createQueryBuilder('shop_type')
+                                ->orderBy()
+                                ->getQuery()
+                                ->getResult();
+    }
+}
